f0e90dea481017aff0d7c4f4bf7e14958785f506
tinali0 <xzl5423@psu.edu>
2020-09-03 00:35:48 +0000

    space + comma

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ unit = input("Enter unit in F/f or C/c: ")
 if unit =="F" or unit == "f":
   temperature=float(temperature)
   celsius=float(temperature-32)*5/9
-  print(str(temperature)+ "°"+ "in Fahrenheit is equivalent to" + str(celsius)+ "°"+ "Celsius")
+  print(str(temperature)+ "°"+ "in Fahrenheit is equivalent to " + str(celsius)+ "°"+ " Celsius")
 elif unit =="C" or unit == "c":
   temperature=float(temperature)
   fahrenheit=float(temperature*9/5+32)
   print(str(temperature) + "°" +" in Celsius is equivalent to "+ str(fahrenheit)+"°"+ " Fahrenheit.")
 else:
-  print(f"Invalid unit({unit})")
+  print(f"Invalid unit({unit}).")
